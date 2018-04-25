8a946d8b2a5c6d0f95cfb4722c970c0e18eb63e1
Tobias Horsmann <tobias.horsmann@gmail.com>
2018-04-25 11:28:32 +0200

    export as fat-jar

--- de.unidue.ltl.gapfill/src/main/java/de/unidue/ltl/gapfill/BundleExample.java
+++ de.unidue.ltl.gapfill/src/main/java/de/unidue/ltl/gapfill/BundleExample.java
@@ -15,17 +15,22 @@ import org.apache.uima.fit.factory.CollectionReaderFactory;
 import de.unidue.ltl.gapfill.indexer.CorpusIndexer;
 import de.unidue.ltl.gapfill.io.LineTokenTagReader;
 import de.unidue.ltl.gapfill.lookup.SubstituteLookup;
+import de.unidue.ltl.gapfill.subsbuilder.FastSubsConnector;
 import de.unidue.ltl.gapfill.util.SubstituteVector;
 
 public class BundleExample
 {
+    static int LIMIT=25;
 
     public static void main(String[] args) throws Exception
     {
         Properties p = new Properties();
-        FileInputStream f = new FileInputStream("src/main/resources/config.txt");
+        FileInputStream f = new FileInputStream(args[0]);
         p.load(f);
         f.close();
+        
+        String word = args[1];
+        String pos = args[2];
 
         String sourceFolder = p.getProperty("folder");
         String model = p.getProperty("model");
@@ -41,27 +46,21 @@ public class BundleExample
         Path lmPath = Paths.get(model);
         Path indexPath = Paths.get(indexLocation);
 
-        CorpusIndexer indexer = new CorpusIndexer(indexPath, reader, preprocessing, 100);
+        CorpusIndexer indexer = new CorpusIndexer(indexPath, reader, preprocessing, LIMIT);
         indexer.index();
 
-        // BaselineSubstituteBuilder subsBuilder = new BaselineSubstituteBuilder(indexPath, 100,
-        // true);
-        // FastSubsConnector subsBuilder = new FastSubsConnector(indexPath, lmPath, 100);
+        System.out.println("Calling FastSubs to build substitutes---");
+         FastSubsConnector subsBuilder = new FastSubsConnector(indexPath, lmPath, LIMIT);
+         subsBuilder.buildSubstitutes();
+         System.out.println("--- done");
 
-        // JWeb1TSubsBuilder s = new JWeb1TSubsBuilder(indexPath, 100);
-        // subsBuilder.buildSubstitutes();
-        // List<SubstituteVector> substitutes = subsBuilder.getSubstitutes("Hallo , heute ist das
-        // Wetter besonders gut ");
-
-        SubstituteLookup sl = new SubstituteLookup(indexPath, 100);
-        List<SubstituteVector> bundle = sl.getBundle(4, "gegeben", "VVPP");
+        System.out.println("Creating bundle ---");
+        SubstituteLookup sl = new SubstituteLookup(indexPath, LIMIT);
+        List<SubstituteVector> bundle = sl.getBundle(4, word, pos);
         for (SubstituteVector s : bundle) {
             System.out.println(s);
         }
-
-        // BaselineSubstituteBuilder subsBuilder = new BaselineSubstituteBuilder(indexPath, 100,
-        // true);
-        // subsBuilder.buildSubstitutes();
+        System.out.println("--- done");
 
     }
 }
